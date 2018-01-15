ec510b0544303685a15e52c4818b217a8d7d87cd
OlgaRomel <lylik.92@gmail.com>
2018-01-15 23:32:49 +0300

    не проверяла работу, позже

---
+++ lessonphp1.php
@@ -0,0 +1,23 @@
+<?php
+
+$login = "admin";
+echo "Вы вошли под именем $login <br>";
+unset($login);
+
+$a = 2;
+$b = 3;
+$res = **$a + **$b;
+echo $res . "<br>";
+
+
+$s = 100;
+$t = 15;
+$v = $s / $t;
+$v2 = $s*1000 / ($t*3600) 
+echo $v . "км/ч или " . $v2 . "м/c <br>" ;
+
+const NAME1 = "admin";
+
+
+
+?>
---
+++ lessonphp2.php
@@ -0,0 +1,17 @@
+<?php
+$arr = ['a', 'b', 'c', 'd', 'e'];
+	$result = array_replace($arr, [0=>'!', 2=>'!!']));
+	var_dump($result);
+	
+$res = array_chunk($arr, 2);
+var_dump($res);
+
+$arr2 = [3, 1, 6, 0, 4, 5];
+foreach ($arr2 as $var) {
+	$res += **$var;
+	echo $res;
+}
+	
+
+
+?>
