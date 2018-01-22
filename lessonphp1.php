c112790b2fcaf38b5f1089df45379a4c6d91b7f5
OlgaRomel <lylik.92@gmail.com>
2018-01-23 00:01:09 +0300

    dz

--- lessonphp1.php
+++ lessonphp1.php
@@ -1,22 +1,28 @@
 <?php
 
 $login = "admin";
-echo "Вы вошли под именем $login <br>";
+echo "You are entered under the name - $login <br>";
 unset($login);
 
 $a = 2;
 $b = 3;
-$res = **$a + **$b;
+$res = $a**2 + $b**2;
 echo $res . "<br>";
 
 
-$s = 100;
-$t = 15;
+$s = 180;
+$t = 10;
 $v = $s / $t;
-$v2 = $s*1000 / ($t*3600) 
-echo $v . "км/ч или " . $v2 . "м/c <br>" ;
+$v2 = $s*1000 / ($t*3600);
+echo $v ." km/h or " .$v2 ." m/s <br>";
 
-const NAME1 = "admin";
+const NAME1 = "admin <br>";
+echo NAME1;
+
+NAME1 == "day";
+
+echo NAME1;
+
 
 
 
--- lessonphp2.php
+++ lessonphp2.php
@@ -1,6 +1,6 @@
 <?php
 $arr = ['a', 'b', 'c', 'd', 'e'];
-	$result = array_replace($arr, [0=>'!', 2=>'!!']));
+	$result = array_replace($arr, [0=>'!', 2=>'!!']);
 	var_dump($result);
 	
 $res = array_chunk($arr, 2);
@@ -8,10 +8,107 @@ var_dump($res);
 
 $arr2 = [3, 1, 6, 0, 4, 5];
 foreach ($arr2 as $var) {
-	$res += **$var;
-	echo $res;
+	$res2 += $var**2;
 }
+echo "resultat zadachi #2 $res2 <br>";
 	
 
+for ($i =1 ; $i<=10; $i++) {
+    echo "<br>";
+    for ($j = 1; $j<=10; $j++){
+        echo $i*$j ." ";
+    }
+}
 
+ echo "<br><br>zd # 4<br><br>";
+
+for ($x=0; $x<15; $x++) {
+    echo "<br>";
+    for ($y=0; $y<=$x; $y++){
+         echo "* ";
+    }
+}
+
+
+$height=15;
+
+//for ($i = 0; $i < $height; $i++)
+//    {
+//        echo "<br>";
+//        for ($j = 1; $j < $height - i; $j++)
+//        {
+//            echo " ";
+//        }   
+// 
+//        for ($j = $height - 2 * $i; $j <= $height; $j++)
+//        {
+//            echo "*";
+//        }           
+//    }
+
+
+ echo "<br><br>zd # 5<br><br>";
+
+$days = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+
+foreach ($days as $day => $val) {
+    if ($day++ == date(N)) {
+        echo "<b> $val </b>  ";
+    } else {
+        echo "$val ";
+    }
+}
+unset($val);
+
+ echo "<br><br>zd # 6<br><br>";
+
+//$arr_num = range(0, 30);
+
+//if ((in_array ("3", ) == !false) && is_int($num/5)) {
+//        
+//}
+
+foreach (range(0, 30) as $num) {
+    //var_dump(strpos($num, "3"));
+    if ((strpos($num, "3") !== false) && !is_int($num/5)){ //($num%5 !== 0)){
+        echo $num ." <br>";
+    }
+}
+
+
+ echo "<br><br>zd # 7<br><br>";
+
+//Отсортировать массив по 'price'
+     
+$arr_price = [
+    '1'=> [
+        'price' => 10,
+        'count' => 2
+        ],
+    '2'=> [
+        'price' => 5,
+        'count' => 5
+        ],
+    '3'=> [
+        'price' => 8,
+        'count' => 5
+        ],
+    '4'=> [
+        'price' => 12,
+        'count' => 4
+        ],
+    '5'=> [
+        'price' => 8,
+        'count' => 4
+        ],
+];
+
+//array_multisort($arr_price)
+
+$sortArr = $arr_price;
+foreach($arr_price as $key=>$val){
+    $sortArr[$key] = $val['price'];
+}
+array_multisort($sortArr,$arr_price);
+var_dump ($arr_price);
 ?>
